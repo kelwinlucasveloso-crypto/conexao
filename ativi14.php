b2244a92d80ed2ab7759d9d2024f52831f4634bc
kelwinlucasveloso-cripto <kelwinlucasveloso@gmail.com>
2026-07-01 11:12:56 -0300

    atividade

--- ativi14.php
+++ ativi14.php
@@ -2,14 +2,14 @@
 
 include "conexao.php";
 
-$numero1 = 99;
-$numero2 = 24;
-$numero3 = 55;
+$numero1 = 1000;
+$numero2 = 500;
+$numero3 = 300;
 
 
-$resultado = $numero1 * $numero2 * $numero3;
+$resultado = $numero1 - $numero2 - $numero3;
 
-$sql = "INSERT INTO Multiplicação (numero1, numero2, numero3,resultado)
+$sql = "INSERT INTO Subtracao (numero1, numero2, numero3,resultado)
 VALUES ($numero1, $numero2, $numero3, $resultado)";
  
 
